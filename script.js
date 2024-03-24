81bb116a97c3dc41e2215f9931259747a6f49540
Zeeshu1708 <zeeshanahmed1708@gmail.com>
2024-03-25 01:01:43 +0530

    dark theme funcnailty funcnality also added project completed

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let scrollContainer = document.querySelector("#scrollContainer");
 let backBtn = document.querySelector("#backBtn");
 let nextBtn = document.querySelector("#nextBtn");
+let icon = document.querySelector("#icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark")
+    if (document.body.classList.contains("dark")) {
+        icon.classList = "ri-sun-fill"
+    }else{
+        icon.classList = "ri-moon-fill"
+    }
+}
+
 
 
 scrollContainer.addEventListener("wheel", (dets) => {
